a14cd4205866acaa65de03727b803f909e9a96ba
Wilfred Githuka <wilfredgachugi@gmail.com>
2017-08-09 05:30:42 +0300

    Kernel bank added arraged in a set of tuples

--- convolutions.py
+++ convolutions.py
@@ -49,3 +49,47 @@ def convolve(image, kernel):
  
 		# return the output image
 		return output
+
+#The Kernels
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True,
+	help="path to the input image")
+args = vars(ap.parse_args())
+ 
+# construct average blurring kernels used to smooth an image
+smallBlur = np.ones((7, 7), dtype="float") * (1.0 / (7 * 7))
+largeBlur = np.ones((21, 21), dtype="float") * (1.0 / (21 * 21))
+ 
+# construct a sharpening filter
+sharpen = np.array((
+	[0, -1, 0],
+	[-1, 5, -1],
+	[0, -1, 0]), dtype="int")
+# construct the Laplacian kernel used to detect edge-like
+# regions of an image
+laplacian = np.array((
+	[0, 1, 0],
+	[1, -4, 1],
+	[0, 1, 0]), dtype="int")
+ 
+# construct the Sobel x-axis kernel
+sobelX = np.array((
+	[-1, 0, 1],
+	[-2, 0, 2],
+	[-1, 0, 1]), dtype="int")
+ 
+# construct the Sobel y-axis kernel
+sobelY = np.array((
+	[-1, -2, -1],
+	[0, 0, 0],
+	[1, 2, 1]), dtype="int")
+# to apply using both our custom `convole` function and
+# OpenCV's `filter2D` function
+kernelBank = (
+	("small_blur", smallBlur),
+	("large_blur", largeBlur),
+	("sharpen", sharpen),
+	("laplacian", laplacian),
+	("sobel_x", sobelX),
+	("sobel_y", sobelY)
